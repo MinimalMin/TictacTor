db2a9a714d94d25ad0fb389f8de36aa1f323f11c
MinimalMin <50867869+MinimalMin@users.noreply.github.com>
2022-03-19 16:28:39 +0700

    Update tictactor.test.js

--- tictactor.test.js
+++ tictactor.test.js
@@ -35,6 +35,51 @@ test('tictactor [[1,2,0],[2,0,1],[0,1,0]] to equal null', () => {
     expect(tictactor([[1,2,1],[2,1,1],[2,1,2]])).toBe('Draw');
   });
 
+const tictactor = require('./tictactor');
+test('tictactor [[1,2,0],[2,0,1],[0,1,0]] to equal null', () => {
+  expect(tictactor([[1,2,0],[2,0,1],[0,1,0]])).toBe(null);
+});
+
+  test('tictactor [[1,1,1],[2,2,1],[2,1,2]] to equal X Win', () => {
+    expect(tictactor([[1,1,1],[2,2,1],[2,1,2]])).toBe('X Win');
+  });
+
+  test('tictactor [[1,2,1],[2,2,2],[2,1,1]] to equal O Win', () => {
+    expect(tictactor([[1,2,1],[2,2,2],[2,1,1]])).toBe('O Win');
+  });
+
+  test('tictactor [[1,2,2],[2,1,1],[1,1,1]] to equal X Win', () => {
+    expect(tictactor([[1,2,2],[2,1,1],[1,1,1]])).toBe('X Win');
+  });
+
+  test('tictactor [[1,2,1],[1,2,1],[1,1,2]] to equal X Win', () => {
+    expect(tictactor([[1,2,1],[1,2,1],[1,1,2]])).toBe('X Win');
+  });
+
+  test('tictactor [[1,2,2],[2,1,2],[1,1,2]] to equal O Win', () => {
+    expect(tictactor([[1,2,2],[2,1,2],[1,1,2]])).toBe('O Win');
+  });
+
+  test('tictactor [[1,2,1],[2,1,1],[1,1,2]] to equal X Win', () => {
+    expect(tictactor([[1,2,1],[2,1,1],[1,1,2]])).toBe('X Win');
+  });
+
+  test('tictactor [[1,2,1],[2,1,1],[2,1,2]] to equal Draw', () => {
+    expect(tictactor([[1,2,1],[2,1,1],[2,1,2]])).toBe('Draw');
+  });
+
+  test('tictactor [[1,2,1],[1,2,1],[1,2,2]] to equal Draw', () => {
+    expect(tictactor([[1,2,1],[2,1,1],[2,1,2]])).toBe('Draw');
+  });
+
+  test('tictactor [[1,2,1],[1,1,1],[2,2,1]] to equal Draw', () => {
+    expect(tictactor([[1,2,1],[2,1,1],[2,1,2]])).toBe('Draw');
+  });
+
+
+  
+  
+  
 
 
   
